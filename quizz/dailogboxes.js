c880fb7d2ef952cdcab60eb7eab95713711d276f
SHasule <surajhasule43@gmail.com>
2024-10-13 19:52:13 +0530

    string

---
+++ quizz/dailogboxes.js
@@ -0,0 +1,11 @@
+
+
+// const alertResult= alert("hello")
+// console.log(alertResult);
+
+// const confirmResult=confirm("would you like to buy coffee?")
+// console.log(confirmResult);
+
+const promptreuslt=prompt("enter your name")
+console.log(promptreuslt);
+
---
+++ quizz/string.js
@@ -0,0 +1,6 @@
+
+
+const lastfourdigit="7620213396";
+lastfourdigit.padStart(10,"*")
+
+lastfourdigit.padEnd(10,"#")
