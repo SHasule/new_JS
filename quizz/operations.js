1592f264a7146ab2ca17ce63d0f5635295cb943d
SHasule <surajhasule43@gmail.com>
2025-01-12 22:17:29 +0530

    first commit

---
+++ quizz/operations.js
@@ -0,0 +1,69 @@
+// console.log("a"+1);
+// console.log(1+"a");
+// console.log("1"+"a");
+// console.log(1+2);
+
+
+// console.log(1+"2"+2);
+// console.log("1"+"2"+2);
+// console.log(1+"2"+"2");
+// console.log(1+2+"2");
+
+// console.log("2"+2+2);
+
+// console.log(  (2+"2"+2));
+// console.log(( '2'- 2+2));
+
+
+// console.log('2'-'2');
+
+
+
+
+ const a=false
+ const b=7
+ console.log(( a==b));
+ console.log(( a===b));
+
+ const c={}
+
+ const d={
+  name:"raj"
+ }
+ 
+  c[d]={
+    name:"suraj"
+  }
+
+  console.log(c[d]);
+  
+ const e={}
+
+ const f={
+  name:"om"
+ }
+
+e[f]={
+  name:"kiran"
+}
+console.log(e[f]);
+
+ 
+ console.log(true == "0");
+ 
+ 
+ 
+
+
+
+ 
+ const result = undefined+"Hello" * 5; // Multiplying a string with a number is invalid
+console.log(result); // NaN
+
+//  const result =  * 5; // Multiplying a string with a number is invalid
+// console.log(result); // NaN
+
+console.log("value");
+
+const value = "abc" / 2; // NaN
+console.log((value)); // true
